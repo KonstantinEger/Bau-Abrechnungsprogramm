f9374640000c72941190ba4714ff3db70d625a63
KonstantinEger <konstantineger@outlook.de>
2020-08-18 14:34:39 +0200

    debug + display project list + send back selected

--- app/scripts/welcome.js
+++ app/scripts/welcome.js
@@ -1,5 +1,9 @@
 (() => {
 
+  window.onmessage = ({ data }) => {
+    console.log('MSG:', data);
+  };
+
   const newProjBtn = document.querySelector('#btn-new');
   const openProjBtn = document.querySelector('#btn-open');
 
@@ -8,7 +12,7 @@
   });
 
   openProjBtn.addEventListener('click', () => {
-    const openProjWin = window.open('./openProject.html', '_blank', 'width=800,height=600');
+    const openProjWin = window.open('./openProject.html', '_blank', 'width=1000,height=600');
   });
 
 })();
