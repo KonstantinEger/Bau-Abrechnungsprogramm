a0abd548c14b62ed4f61a877986776c6bb3fdb47
KonstantinEger <35273325+KonstantinEger@users.noreply.github.com>
2020-10-25 13:18:58 +0100

    tabs and snake_case files

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -11,76 +11,76 @@ const { remote } = require('electron');
  * }} project 
  */
 async function openProject(project) {
-  if (!project) return;
+	if (!project) return;
 
-  document.body.innerHTML = '';
-  document.title = 'Bau-Abrechnungen | Projekt | ' + project.name;
+	document.body.innerHTML = '';
+	document.title = 'Bau-Abrechnungen | Projekt | ' + project.name;
 
-  const { promises: fs } = require('fs');
-  const { join } = require('path');
+	const { promises: fs } = require('fs');
+	const { join } = require('path');
 
-  const styleLink = document.createElement('link');
-  styleLink.rel = 'stylesheet';
-  styleLink.href = join(__dirname, './styles/project.css');
-  document.head.appendChild(styleLink);
+	const styleLink = document.createElement('link');
+	styleLink.rel = 'stylesheet';
+	styleLink.href = join(__dirname, './styles/project.css');
+	document.head.appendChild(styleLink);
 
-  const htmlSrcPath = join(__dirname, './projectTemplate.html');
-  document.body.innerHTML = await fs.readFile(htmlSrcPath, 'utf8');
+	const htmlSrcPath = join(__dirname, './project_template.html');
+	document.body.innerHTML = await fs.readFile(htmlSrcPath, 'utf8');
 
-  document.getElementById('project-name-display').textContent = project.name;
-  document.getElementById('project-place-display').textContent = project.place;
-  document.getElementById('project-date-display').textContent = project.date;
-  document.getElementById('notes-input').value = project.notes;
+	document.getElementById('project-name-display').textContent = project.name;
+	document.getElementById('project-place-display').textContent = project.place;
+	document.getElementById('project-date-display').textContent = project.date;
+	document.getElementById('notes-input').value = project.notes;
 }
 
 (() => {
 
-  window.onmessage = async ({ data }) => {
-    if (data.name === 'OPEN_PROJECT') {
-      await openProject(data.project);
-      return;
-    }
-  };
+	window.onmessage = async ({ data }) => {
+		if (data.name === 'OPEN_PROJECT') {
+			await openProject(data.project);
+			return;
+		}
+	};
 
-  const newProjBtn = document.querySelector('#btn-new');
-  const openProjBtn = document.querySelector('#btn-open');
+	const newProjBtn = document.querySelector('#btn-new');
+	const openProjBtn = document.querySelector('#btn-open');
 
-  newProjBtn.addEventListener('click', () => {
-    window.open('./newProject.html', '_blank', 'width=800,height=600');
-  });
+	newProjBtn.addEventListener('click', () => {
+		window.open('./new_project.html', '_blank', 'width=800,height=600');
+	});
 
-  openProjBtn.addEventListener('click', async () => {
-    // https://www.brainbell.com/javascript/show-open-dialog.html#show-open-dialog
+	openProjBtn.addEventListener('click', async () => {
+		// https://www.brainbell.com/javascript/show-open-dialog.html#show-open-dialog
 
-    const dialog = remote.dialog;
-    const browserWin = remote.getCurrentWindow();
+		const dialog = remote.dialog;
+		const browserWin = remote.getCurrentWindow();
 
-    let opts = {
-      title: 'Bauprojekt öffnen',
-      defaultPath: process.env.HOME || process.env.HOMEPATH,
-      buttonLabel: 'Öffnen',
-      filters: [
-        {name: 'Bauprojekt', extensions: ['tbvp.csv']},
-        {name: 'CSV', extensions: ['csv']},
-        {name: 'Alle Datein', extensions: ['*']}
-      ],
-      properties: ['openFile']
-    };
+		let opts = {
+			title: 'Bauprojekt öffnen',
+			defaultPath: process.env.HOME || process.env.HOMEPATH,
+			buttonLabel: 'Öffnen',
+			filters: [
+				{name: 'Bauprojekt', extensions: ['tbvp.csv']},
+				{name: 'CSV', extensions: ['csv']},
+				{name: 'Alle Datein', extensions: ['*']}
+			],
+			properties: ['openFile']
+		};
 
-    const { canceled, filePaths } = await dialog.showOpenDialog(browserWin, opts);
-    if (canceled || filePaths.length === 0) return;
+		const { canceled, filePaths } = await dialog.showOpenDialog(browserWin, opts);
+		if (canceled || filePaths.length === 0) return;
 
-    // TODO: read and parse file
-    // call 'openProject' with data
-  });
+		// TODO: read and parse file
+		// call 'openProject' with data
+	});
 
-  // uncomment next call to skip project seletion on start
-  // openProject({
-  //   name: '%projectname%',
-  //   id: 0,
-  //   place: '%place%',
-  //   date: '%date%',
-  //   notes: '%notes%'
-  // });
+	// uncomment next call to skip project seletion on start
+	// openProject({
+	//   name: '%projectname%',
+	//   id: 0,
+	//   place: '%place%',
+	//   date: '%date%',
+	//   notes: '%notes%'
+	// });
 
 })();
--- app/scripts/new_project.js
+++ app/scripts/new_project.js
@@ -1,69 +1,69 @@
 const { remote } = require('electron');
 
 document.getElementById('create-project-btn').addEventListener('click', async () => {
-  const nameInput = document.getElementById('project-name-input');
-  const placeInput = document.getElementById('project-place-input');
-  const dateInput = document.getElementById('project-date-input');
-  const notesInput = document.getElementById('project-notes-input');
-  nameInput.classList.remove('invalid');
-  placeInput.classList.remove('invalid');
-  dateInput.classList.remove('invalid');
+	const nameInput = document.getElementById('project-name-input');
+	const placeInput = document.getElementById('project-place-input');
+	const dateInput = document.getElementById('project-date-input');
+	const notesInput = document.getElementById('project-notes-input');
+	nameInput.classList.remove('invalid');
+	placeInput.classList.remove('invalid');
+	dateInput.classList.remove('invalid');
 
-  {
-    let exitDueToError = false;
-    if (nameInput.value.length === 0) {
-      nameInput.classList.add('invalid');
-      exitDueToError = true;
-    }
-  
-    if (placeInput.value.length === 0) {
-      placeInput.classList.add('invalid');
-      exitDueToError = true;
-    }
-  
-    if (dateInput.value.length === 0) {
-      dateInput.classList.add('invalid');
-      exitDueToError = true;
-    }
-  
-    if (exitDueToError) return;
-  }
+	{
+		let exitDueToError = false;
+		if (nameInput.value.length === 0) {
+			nameInput.classList.add('invalid');
+			exitDueToError = true;
+		}
+	
+		if (placeInput.value.length === 0) {
+			placeInput.classList.add('invalid');
+			exitDueToError = true;
+		}
+	
+		if (dateInput.value.length === 0) {
+			dateInput.classList.add('invalid');
+			exitDueToError = true;
+		}
+	
+		if (exitDueToError) return;
+	}
 
-  // TODO: generate ID
-  const id = 6;
+	// TODO: generate ID
+	const id = 6;
 
-  const dialog = remote.dialog;
-  const browserWin = remote.getCurrentWindow();
+	const dialog = remote.dialog;
+	const browserWin = remote.getCurrentWindow();
 
-  // https://www.brainbell.com/javascript/show-save-dialog.html
+	// https://www.brainbell.com/javascript/show-save-dialog.html
 
-  let opts = {
-    title: 'Neues Projekt speichern',
-    defaultPath : (process.env.HOME || process.env.HOMEPATH) + `\\${nameInput.value}.tbvp.csv`,
-    buttonLabel : 'Neues Bauprojekt Speichern',
-    filters :[
-      {name: 'Bauprojekt', extensions: ['tbvp.csv']},
-      {name: 'Alle Datein', extensions: ['*']}
-    ]
-  }
+	let opts = {
+		title: 'Neues Projekt speichern',
+		defaultPath : (process.env.HOME || process.env.HOMEPATH) + `\\${nameInput.value}.tbvp.csv`,
+		buttonLabel : 'Neues Bauprojekt Speichern',
+		filters :[
+			{name: 'Bauprojekt', extensions: ['tbvp.csv']},
+			{name: 'Alle Datein', extensions: ['*']}
+		]
+	}
 
-  let { canceled, filePath } = await dialog.showSaveDialog(browserWin, opts);
-  if (canceled || !filePath) return;
+	let { canceled, filePath } = await dialog.showSaveDialog(browserWin, opts);
+	if (canceled || !filePath) return;
 
-  // TODO: Write to disk
+	// TODO: Write to disk
 
-  window.opener.postMessage({
-    name: 'OPEN_PROJECT',
-    project: {
-      id,
-      name: nameInput.value,
-      date: dateInput.value,
-      place: placeInput.value,
-      notes: notesInput.value
-    }
-  });
+	window.opener.postMessage({
+		name: 'OPEN_PROJECT',
+		project: {
+			id,
+			name: nameInput.value,
+			date: dateInput.value,
+			place: placeInput.value,
+			notes: notesInput.value
+		}
+	});
 
-  window.close();
+	window.close();
 });
 
 document.getElementById('close-window-btn').addEventListener('click', window.close);
