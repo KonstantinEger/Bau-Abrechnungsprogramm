9d36481d54a67322aa41556ef16205c41106eaa5
KonstantinEger <35273325+KonstantinEger@users.noreply.github.com>
2021-01-03 23:30:51 +0100

    debugging

--- rollup.config.js
+++ rollup.config.js
@@ -27,36 +27,40 @@ export default [
 		input: './app/scripts/src/index.ts',
 		output: {
 			file: './app/scripts/dist/main-bundle.js',
-			format: commons.format
+			format: commons.format,
+            sourcemap: true
 		},
         plugins: commons.plugins,
-        external: commons.externals
+        external: commons.externals,
 	},
 	{
 		input: './app/scripts/src/new_project.ts',
 		output: {
 			file: './app/scripts/dist/new-project-bundle.js',
-			format: commons.format
+			format: commons.format,
+            sourcemap: true
 		},
         plugins: commons.plugins,
-        external: commons.externals
+        external: commons.externals,
 	},
 	{
 		input: './app/scripts/src/new_material.ts',
 		output: {
 			file: './app/scripts/dist/new-material-bundle.js',
-			format: commons.format
+			format: commons.format,
+            sourcemap: true
 		},
         plugins: commons.plugins,
-        external: commons.externals
+        external: commons.externals,
 	},
 	{
 		input: './app/scripts/src/new_worker_type.ts',
 		output: {
 			file: './app/scripts/dist/new-worker-bundle.js',
-			format: commons.format
+			format: commons.format,
+            sourcemap: true
 		},
         plugins: commons.plugins,
-        external: commons.externals
+        external: commons.externals,
 	}
 ]
